8b79111702768aedacb0379a431f8adbd43b3672
Griffin McCormick <gmccormi@umich.edu>
2018-10-26 15:07:33 -0400

    added error handling for too many operands

--- rpn.py
+++ rpn.py
@@ -15,11 +15,17 @@ def calculate(arg):
             elif token == '-':
                 result = val1 - val2
             stack.append(result)
+
+    if len(stack) > 1:
+        raise ValueError('Too many arguments on the stack')
     return stack[0]
 
 def main():
     while True:
-        print(calculate(input("rpn calc> ")))
+        try:
+            print(calculate(input("rpn calc> ")))
+        except ValueError:
+            pass
 
 if __name__ == '__main__':
     main()
